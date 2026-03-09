2c36c4afa12a427da2c5c9512fc688c59c8e1824
Oleksandr <sashapylypenko.pov@gmail.com>
2026-03-09 19:04:17 +0200

    fix: daniel

--- daniel/script.js
+++ daniel/script.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".article-but").forEach(button => button.addEventListener("click", function () {
         alert('Сторінка товару ще в розробці!');
     }));
+
+    document.querySelectorAll(".nav-link").forEach(button => button.addEventListener("click", function () {
+        alert('Ця сторінка ще в розробці!');
+    }));
 });
